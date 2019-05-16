ecb84e6c4c45e4040c720a1bfe995b26c61524a5
unknown <rafik.mohammad1994@gmail.com>
2019-05-16 16:14:48 -0400

    created a  registeration form template

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    return render(request, 'accounts/register.html')
+
+def submit_register(request):
+    return render(request, 'accounts/test.html')
