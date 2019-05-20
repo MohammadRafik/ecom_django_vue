1b779d28b9d82a01c54739087c1b93767bdb66de
unknown <rafik.mohammad1994@gmail.com>
2019-05-19 23:45:28 -0400

    improved registeration logic

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -3,15 +3,19 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegistrationForm(UserCreationForm):
+    # username = forms.CharField(length=100)
     email = forms.EmailField(required=True)
-
+    password1 = forms.CharField(min_length=8,max_length=100)
+    password2 = forms.CharField(min_length=8,max_length=100)
+    
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2',)
     
     def save(self, commit=True):
         user = super(RegistrationForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
+        user.username = self.cleaned_data['username']
 
         if commit:
             user.save()
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -3,17 +3,15 @@ from accounts.forms import RegistrationForm
 
 # Create your views here.
 def register(request):
-    return render(request, 'accounts/register.html')
 
-def submit_register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('admin/auth/user')
-
+            return redirect('accounts')
     else:
         form = RegistrationForm()
+    
+    args = { 'form': form }
+    return render(request, 'accounts/register.html', args)
 
-        args = { 'form': form }
-        return render(request, 'accounts/register.html', args)
