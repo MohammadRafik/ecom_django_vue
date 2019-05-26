f46d71e2e8f71a906a66c27009f5164a49497b90
unknown <rafik.mohammad1994@gmail.com>
2019-05-26 18:50:57 -0400

    added login logic and template

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('register', views.Register.as_view(), name='register'),
+    path('login', views.Login.as_view(), name='login'),
+    
 
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from accounts.forms import RegistrationForm
 from django.views import View
+from django.contrib.auth import authenticate, login, logout, get_user_model
 
 # Create your views here.
 class Register(View):
@@ -15,9 +16,35 @@ class Register(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             form.save()
+            
             return redirect('accounts')
         else:
             return render(request, self.template_name, {'form':form})
 
+class Login(View):
+    template_name = 'accounts/login.html'
 
-        
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self,request):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        User = get_user_model()
+        if '@' in username:
+            try:
+                current_user = User.objects.get(email__iexact=username)
+                username = current_user.username
+            except:
+                error = "Oops! something's wrong. We couldn't find that email"
+                return render(request, self.template_name, {'error':error, 'form_username':username})
+
+        user = authenticate(username=username,password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('accounts')
+        else:
+            error = "Oops! something's wrong. Your username/email and password didnt match"
+            return render(request, self.template_name,  {'error':error, 'form_username':username})
