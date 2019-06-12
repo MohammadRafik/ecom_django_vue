983f39ec9d427d2056b7441e8d7b1e20517f0aed
unknown <rafik.mohammad1994@gmail.com>
2019-06-12 17:22:56 -0400

    renamed gatalog to pageloader

--- src/pageloader/views.py
+++ src/pageloader/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-class BaseCatalog(View):
+class BaseLoader(View):
     # this class is used to find all catagorys in the database bring them
     # in, and check if a catagory is selected and load things accordingly
 
