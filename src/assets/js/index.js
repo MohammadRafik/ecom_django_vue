d1d46dcb72b59b25d739ce7657f558b4333dced1
unknown <rafik.mohammad1994@gmail.com>
2019-06-20 12:57:49 -0400

    added a dropdown vue component that will be used for navigating the categories

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,11 +3,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Vue from 'vue';
+
+// components from github
+import Dropdown from 'bp-vuejs-dropdown';
+
+// my custom components
 import Demo from "./components/Demo.vue";
 
 const app = new Vue({
     el: '#app',
     components: {
-        Demo
+        Demo,
+        Dropdown
     }
 });
