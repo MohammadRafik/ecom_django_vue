2ac4011f7e6da526dd2ce4950d2567992345bbb6
MohammadRafik <rafik.mohammad1994@gmail.com>
2019-11-16 17:39:01 -0800

    successfully integrated axios with the front end to make http requests to the products api

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,21 +1,25 @@
 window.$ = window.jQuery = require('jquery');
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import Vue from 'vue';
+// importing axios needs to be done on each component seperatly
+const axios = require('axios').default;
 
-// components from github
+
+// components
 import Dropdown from 'bp-vuejs-dropdown';
 
 // my custom components
 import Demo from "./components/Demo.vue";
+import Navigation from "./components/navigation.vue"
 
 const app = new Vue({
     el: '#app',
-    data: data,
+    // data: data,
     delimiters: ["[[","]]"],
     components: {
         Demo,
-        Dropdown
+        Dropdown,
+        Navigation
     }
 });
