c734d0fe089e16ca9041febd101fc3fa8ad8b9db
MohammadRafik <rafik.mohammad1994@gmail.com>
2020-01-03 14:29:34 -0800

    added functions to calcuate costs of items

--- src/cart/models.py
+++ src/cart/models.py
@@ -3,6 +3,7 @@ from products.models import Product
 
 
 # Create your models here.
+#add functionality to be able to remove a cartItem, or change its quantity
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
@@ -10,6 +11,11 @@ class CartItem(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.CharField(max_length=100)
+
+    def find_cost(self):
+        self.current_price = self.product.current_price
+        tax = 1.12
+        self.total_cost = self.quantity * self.current_price * tax
 
 
 
