1590689c84843075155a72e18929ccc492942bf1
MohammadRafik <rafik.mohammad1994@gmail.com>
2020-01-02 23:39:52 -0800

    created cartItem, and Cart models

--- src/cart/models.py
+++ src/cart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from products.models import Product
+
 
 # Create your models here.
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    updated_by = models.CharField(max_length=100)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=100)
+
+
+
+class Cart(models.Model):
+    cart_item = models.ForeignKey(CartItem, on_delete=models.CASCADE)
+    updated_by = models.CharField(max_length=100)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    created_by = models.CharField(max_length=100)
+
