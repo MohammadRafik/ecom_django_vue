6bac1b720637eef9740e8b163384ac55c672f008
MohammadRafik <rafik.mohammad1994@gmail.com>
2020-01-22 16:00:13 -0800

    created a order confirmation page that shows total money spent and everything u bought, i should add the stripe api response at the bottom just to show it

--- src/cart/urls.py
+++ src/cart/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'cart'
 urlpatterns = [
     path('', views.CartPageLoader.as_view(), name='cartpageloader'),
+    path('confirmation', views.order_confirmation, name='order_confirmation'),
 
 ]
--- src/cart/views.py
+++ src/cart/views.py
@@ -69,7 +69,6 @@ class CartPageLoader(View):
         return render(request, 'cart/home.html', {'cart':self.cart, 'cart_items':cart_items, 'product_images':product_images, 'total_cost':total_cost,'tax':tax, 'total_cost_with_tax':total_cost_with_tax, 'stripe_key':stripe_key, 'total_cost_for_stripe':total_cost_for_stripe})
 
 
-
     def find_total_cart_items(self):
         pass
 
@@ -77,8 +76,43 @@ class CartPageLoader(View):
     def delete_expired_carts(cls):
         pass
 
+def order_confirmation(request):
+    cart = Cart.get_cart(request.session['cart_id'])
+    for the_cart in cart:
+            cart_items = the_cart.get_items()
+    # load main image for each cart item product
+    product_images =  []
+    repeated = False
+    if cart_items:
+        for cart_item in cart_items:
+            img_in_list = list(ProductImage.find_main_product_image(cart_item.product.id))
+            repeated = False
+            for product_image in product_images:
+                if product_image.pk == img_in_list[0].pk:
+                    repeated = True
+                    break
+            if not repeated:
+                product_images += img_in_list
+                repeated = False
+
+    # calculate total cost
+    total_cost = 0.0
+    if cart_items:
+        for cart_item in cart_items:
+            total_cost += (float(cart_item.product.current_price) * cart_item.quantity)
+    total_cost = round(total_cost, 2)
+    tax = total_cost*0.13
+    tax = round(tax, 2)
+    total_cost_with_tax = total_cost*1.13
+    total_cost_with_tax = round(total_cost_with_tax, 2)
+    total_cost_for_stripe = total_cost_with_tax*100
 
 
+    # clear out cart session and make new cart
+    new_cart = Cart.get_cart()
+    for one_cart in new_cart:
+        request.session['cart_id'] = one_cart.id
+    return render(request, 'cart/order_confirmation.html', {'cart':cart, 'cart_items':cart_items, 'product_images':product_images, 'total_cost':total_cost,'tax':tax, 'total_cost_with_tax':total_cost_with_tax, 'total_cost_for_stripe':total_cost_for_stripe})
 
 from rest_framework import viewsets
 from .serializers import CartItemSerializer, CartSerializer
