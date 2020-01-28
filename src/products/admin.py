444d58030633bc8c1ede151b72d7bfb7f41bf26c
MohammadRafik <rafik.mohammad1994@gmail.com>
2020-01-27 18:18:28 -0800

    created new models for featured products

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Product, Category, ProductImage, Supplier
+from .models import Product, Category, ProductImage, Supplier,FeaturedProduct, FeaturedProductImage
 
 admin.site.register(Supplier)
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(ProductImage)
+admin.site.register(FeaturedProduct)
+admin.site.register(FeaturedProductImage)
